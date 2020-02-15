92fc2908aa470537b3cb3cba6620c1b69f9345dd
Alexey Shabginov <a.shabginov@gmail.com>
2020-02-14 19:50:52 -0800

    Completed counter maker assignment

--- index.js
+++ index.js
@@ -259,10 +259,11 @@ function tallyUpDonations(arr) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
+  let count = 0;
   function counter() {
-    ++count
+    return count++;
   }
+  return counter;
   // BROKEN CODE ENDS
 }
 
